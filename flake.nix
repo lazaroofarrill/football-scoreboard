e4ba37f57f901cb6157034edfc7bd872ee7ff9a9
Lazaro O'Farrill <lazaroofarrill@gmail.com>
2026-03-31 22:38:55 +0200

    feat: first commit
    Signed-off-by: Lazaro O'Farrill <lazaroofarrill@gmail.com>

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Java 21 development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [ jdk21 gradle jdt-language-server ];
+
+          shellHook = ''
+            echo "Java $(java -version 2>&1 | head -1)"
+          '';
+        };
+      });
+}
